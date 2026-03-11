856dd92c561c981b64827383921d6f115b17b109
KnowSea <2069571638@qq.com>
2026-03-11 02:20:24 -0700

    2

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.VITE_APP_PUBLIC_PATH),
+  history: createWebHistory(),
   routes: [
     {
       path: '/',
